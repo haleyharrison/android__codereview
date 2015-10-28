004d5c9bb7e09988147ae07ed3e7e671f969e2b8
haleyharrison <harrison.haley@gmail.com>
2015-10-28 14:49:02 -0700

    added pages to game

---
+++ app/src/main/java/com/example/guest/interactivestory/model/Choice.java
@@ -0,0 +1,13 @@
+package com.example.guest.interactivestory.model;
+
+
+
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+}
---
+++ app/src/main/java/com/example/guest/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,33 @@
+package com.example.guest.interactivestory.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.guest.interactivestory.R;
+import com.example.guest.interactivestory.model.Page;
+
+public class StoryActivity extends AppCompatActivity {
+
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+
+        if (name == null) {
+            name = "Friend";
+        }
+
+        Log.d(TAG, name);
+
+
+    }
+
+}
